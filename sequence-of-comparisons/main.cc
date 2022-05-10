89bae1ee549511cb9d56030a778cc495defd42dc
lunbun <53448214+lunbun@users.noreply.github.com>
2022-05-10 22:56:53 +0000

    sequence of comparisons

---
+++ sequence-of-comparisons/main.cc
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <stdexcept>
+
+int main() {
+    int32_t case_count;
+    std::cin >> case_count;
+    std::vector<char> output;
+
+    for (int32_t i = 0; i < case_count; ++i) {
+        char buf;
+        char result = '=';
+        
+        std::string line;
+        std::cin >> line;
+        std::stringstream ss(line);
+
+        while (ss >> buf) {
+            if (buf == '<') {
+                if (result == '<' || result == '=') {
+                    result = '<';
+                    continue;
+                } else if (result == '>') {
+                    result = '?';
+                    break;
+                }
+            } else if (buf == '=') {
+                // No change
+                continue;
+            } else if (buf == '>') {
+                if (result == '<') {
+                    result = '?';
+                    break;
+                } else if (result == '=' || result == '>') {
+                    result = '>';
+                    continue;
+                }
+            }
+
+            // Should not be reached unless input is malformed.
+            throw std::runtime_error(std::string("unknown combination ") + buf + " " + result);
+        }
+        output.push_back(result);
+    }
+
+    for (char c : output) {
+        std::cout << c << std::endl;
+    }
+}
